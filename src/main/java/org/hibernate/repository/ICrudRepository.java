a5ed080344377f6c4a190c604f1612456d40398a
Samet Akbal <39596965+sametakbal@users.noreply.github.com>
2023-04-28 22:56:32 +0300

    save

--- src/main/java/org/hibernate/repository/ICrudRepository.java
+++
@@ -1,9 +0,0 @@
-package org.hibernate.repository;
-
-import java.util.List;
-import java.util.Optional;
-
-public interface ICrudRepository <T,ID>{
-    List<T> findAll();
-    Optional<T> findById(ID id);
-}
